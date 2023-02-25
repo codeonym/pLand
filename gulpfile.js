f2daf2e2176f98b28e86afc44a5f176666acfdfd
kiraXdesu <bouarourayoub0@gmail.com>
2023-02-25 22:44:33 +0100

    automating tasks --major-fix

--- gulpfile.js
+++ gulpfile.js
@@ -14,67 +14,119 @@ const rename = require("gulp-rename");
 const source = require("vinyl-source-stream");
 const buffer = require("vinyl-buffer");
 const uglify = require("gulp-uglify");
+const eventStream = require("event-stream");
 
 //stylesheet paths
 let cssSRC="main.scss";
-let cssFolder="src/stylesheets/";
+let cssFolder="./src/stylesheets/";
 let cssDist="./dist/css/";
 let cssFiles=[cssSRC];
 let cssWatch={
-    css:"src/stylesheets/**/*.css",
-    scss:"src/stylesheets/**/*.scss"
+    css:"./src/stylesheets/**/*.css",
+    scss:"./src/stylesheets/**/*.scss"
 };
 
 //scripts paths
 let  jsSRC="main.js";
-let jsFolder="src/js/";
+let jsFolder="./src/js/";
 let jsDist="./dist/js/";
 let jsFiles=[jsSRC];
-let jsWatch="src/js/**/*.js";
+let jsWatch="./src/js/**/*.js";
 
 // html paths
 let htmlSRC="main.pug";
-let htmlFolder="src/html/";
+let htmlFolder="./src/html/";
 let htmlDist="./dist/";
 let htmlFiles=[htmlSRC];
-let htmlWatch="src/html/**/*.pug";
+let htmlWatch="./src/html/**/*.pug";
+
+// loop:watch tasks
+function loop_tasks(task,watchPath,entries){
+    entries.forEach((entry)=>{
+        gulp.watch(watchPath,gulp.series(`${task}_${entry}`));
+    })
+
+}
+
+    /*
+    **tasks defined functions
+    */
+
+//html task function
+// function htmlTask(entry){
+//     return gulp.src(htmlFolder + entry)
+//         .pipe(pug({pretty: true}))
+//         .pipe(gulp.dest(htmlDist));
+// }
+
+//style task function
+// function styleTask(entry){
+//     return  gulp.src(cssFolder + entry)
+//         .pipe(sourcemaps.init())
+//         .pipe(sass({
+//             outputStyle :'compressed',
+//             errorLogToConsole:true,
+//         }))
+//         .pipe(auto())
+//         .on("error",console.error.bind(console))
+//         .pipe(rename({suffix: '.min'}))
+//         .pipe(sourcemaps.write("./"))
+//         .pipe(gulp.dest(cssDist));
+// }
+
+//script task function
+// function scriptTask(entry){
+//     return browserify({
+//         entries: [jsFolder + entry]
+//     }).transform(babelify, {
+//         presets: ["env"]
+//     }).bundle()
+//         .pipe(source(entry))
+//         .pipe(rename({extname: ".min.js"}))
+//         .pipe(buffer())
+//         .pipe(sourcemaps.init({loadMaps: true}))
+//         .pipe(uglify())
+//         .pipe(sourcemaps.write("./"))
+//         .pipe(gulp.dest(jsDist));
+// }
 
 // html task
-gulp.task('html',()=> {
-    htmlFiles.map((entry)=>{
-       return gulp.src(htmlFolder + entry)
-           .pipe(pug({pretty: true}))
-           .pipe(gulp.dest(htmlDist));
+htmlFiles.map((entry)=> {
+    gulp.task(`html_${entry}`,function(){
+            return gulp.src(htmlFolder + entry)
+                .pipe(pug({pretty: true}))
+                .pipe(gulp.dest(htmlDist));
     });
 });
 
-// css task
-gulp.task("css", ()=>{
-    cssFiles.map((entry)=>{
-       return gulp.src(cssFolder + entry)
-           .pipe(sourcemaps.init())
-           .pipe(sass({
-               outputStyle :'compressed',
-               errorLogToConsole:true,
-           }))
-           .pipe(auto())
-           .on("error",console.error.bind(console))
-           .pipe(rename({suffix: '.min'}))
-           .pipe(sourcemaps.write("./"))
-           .pipe(gulp.dest(cssDist));
+// styles task
+cssFiles.map((entry)=> {
+    gulp.task(`style_${entry}`,function(){
+        return  gulp.src(cssFolder + entry)
+            .pipe(sourcemaps.init())
+            .pipe(sass({
+                outputStyle :'compressed',
+                errorLogToConsole:true,
+            }))
+            .pipe(auto())
+            .on("error",console.error.bind(console))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(sourcemaps.write("./"))
+            .pipe(gulp.dest(cssDist));
     });
-})
+});
 
 // scripts task
-gulp.task("scripts", ()=>{
-    jsFiles.map((entry)=>{
+
+jsFiles.map((entry)=> {
+    gulp.task(`script_${entry}`,function(){
         return browserify({
-            entries:[jsFolder + entry]
-        }).transform(babelify,{
-            presets:["env"]
+            entries: [jsFolder + entry]
+        }).transform(babelify, {
+            presets: ["env"]
         }).bundle()
-        .pipe(source(entry))
-        .pipe(rename({extname:".min.js"}))
+            .pipe(source(entry))
+            .pipe(rename({extname: ".min.js"}))
             .pipe(buffer())
             .pipe(sourcemaps.init({loadMaps: true}))
             .pipe(uglify())
@@ -82,7 +134,6 @@ gulp.task("scripts", ()=>{
             .pipe(gulp.dest(jsDist));
     });
 });
-
 
 // connect task
 gulp.task("connect", ()=> {
@@ -93,13 +144,11 @@ gulp.task("connect", ()=> {
 })
 
 // watch task
-gulp.task("watch", ()=>{
-    //livereload everytime something changes in those paths
-    gulp.watch(htmlWatch,gulp.series("html"));
-    gulp.watch(cssWatch.css,gulp.series("css"));
-    gulp.watch(cssWatch.scss,gulp.series("css"));
-    gulp.watch(jsWatch,gulp.series("scripts"));
-})
-
+gulp.task('watch',()=>{
+    loop_tasks("html",htmlWatch,htmlFiles);
+    loop_tasks("style",cssWatch.scss,cssFiles);
+    loop_tasks("style",cssWatch.css,cssFiles);
+    loop_tasks("script",jsWatch,jsFiles);
+});
 //set gulp default tasks to connect and watch => livereload
-gulp.task('default', gulp.parallel('connect',"watch"));
+gulp.task("default",gulp.parallel("watch","connect"));
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -22,6 +22,7 @@ export default function swiper_func(){
    //
    //  });
    //  //swiper.init();
+
     console.log("reached swiper")
 
 
